24f45d921a2963054609cff516242e908ba8b965
Samarth Shendre <samarthscoe@gmail.com>
2025-05-07 02:41:02 +0530

    commit

--- 04_n_queens_backtracking.py
+++ 04_n_queens_backtracking.py
@@ -1,58 +1,30 @@
-def solveNQueens(n: int):
-    res = []
-    board = [['.' for _ in range(n)] for _ in range(n)]
-
-    def isSafe(row, col):
-        # check in column
-        for i in range(row):    
-            if (board[i][col] == 'Q'):
-                return False
-            
-        # check in positive diagonal
-        i = row
-        j = col
-        while (i >= 0 and j < n):
-            if (board[i][j] == 'Q'):
-                return False
-            i -= 1
-            j += 1
-
-        # check in negative diagonal
-        i = row
-        j = col
-        while (i >= 0 and j >= 0):
-            if (board[i][j] == 'Q'):
-                return False
-            i -= 1
-            j -= 1
-
-        return True
-
-    def backtrack(row):
-        if (row == n):
-            res.append([''.join(row) for row in board])
+def is_safe(board,row,col):
+    for i in range(row):
+        if board[i]==col or abs(board[i]-col)==row-i:
             return False
-        for col in range(n):
-            if (isSafe(row, col)):
-                board[row][col] = 'Q'
-                possible = backtrack(row+1)
-                if (possible):
-                    return True
-                board[row][col] = '.'
-        return False
-    
-    backtrack(0)
-    return res
+    return True
 
-def printSolutions(boards):
-    for board in enumerate(boards):
-        print(f"Solution {board[0]+1}")
-        for row in board[1]:
-            for col in row:
-                print(col, end=' ')
-            print()
-        print()
 
-if __name__ == "__main__":
-    boards = solveNQueens(4)
-    printSolutions(boards)
+def print_board(board,n):
+    for i in range(n):
+        line=["Q"if board[i]==j else "."for j in range(n)]
+        print(" ".join(line))
+
+def solve(board,row,n):
+    if row==n:
+        return True
+    for col in range(n):
+        if is_safe(board,row,col):
+            board[row]=col
+            if solve(board,row+1,n):
+                return True
+            board[row]=-1
+    return False
+
+def n_queen(n):
+    board=[-1]*n
+    if solve(board,0,n):
+        print_board(board,n)
+    else:
+        print("not solvable")
+n_queen(4)
